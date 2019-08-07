664658662e3feb8b50bfbf0ee91801e5a7f4c845
EmilLindfors <emillindfors@hotmail.com>
2019-08-07 10:58:02 +0200

    fixed export

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-export { default as Button } from "./button"
-export { Input } from "./form"
-export { Section, DividedSection } from "./section"
-export { default as TopLayout } from "./top-layout"
-export { Header, HeaderLinks } from "./header"
-export { default as WrapWithProvider } from "./wrap-with-provider"
-export { Hero } from "./hero"
+export { default as Button } from "./button";
+export { Input } from "./form";
+export { Section, DividedSection } from "./section";
+export { default as TopLayout } from "./top-layout";
+export { Header, HeaderLinks } from "./header";
+export { default as WrapWithProvider } from "./wrap-with-provider";
+export { Hero } from "./hero";
 export { Text, Title } from "./typography";
-export { default as TabPill } from "./TabPill";
+export { default as TabPill } from "./tabpill";
 export { default as Badge } from "./badge";
